adb00257e566aa2fcd4309ea066850289bbb0798
Grraoo <evil@grraoo.ru>
2017-10-11 12:14:18 +0800

    заполнил инфу рыбой

--- config.js
+++ config.js
@@ -5,12 +5,12 @@ var regions = {
       hostel: {
         href: '#',
         linkText: 'Иркутский хостел',
-        text: 'Информация про хостел блаблабла'
+        text: 'Информация про иркутский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
       },
       apart: {
         href: '#',
         linkText: 'Иркутский апарт-отель',
-        text: 'Информация про апарт-отель блаблабла-бла-бла!'
+        text: 'Информация про иркутский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
       }
     }
   },
@@ -20,34 +20,245 @@ var regions = {
       hostel: {
         href: '#',
         linkText: 'Красноярский хостел',
-        text: 'Информация про хостел блаблабла'
+        text: 'Информация про красноярский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
       },
       apart: {
         href: '#',
         linkText: 'Красноярский апарт-отель',
-        text: 'Информация про апарт-отель блаблабла-бла-бла!'
+        text: 'Информация про красноярский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      }
+    }
+  },
+  omsk: {
+    name: "г. Омск",
+    bubble: {
+      hostel: {
+        href: '#',
+        linkText: 'Омский хостел',
+        text: 'Информация про Омский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      },
+      apart: {
+        href: '#',
+        linkText: 'Омский апарт-отель',
+        text: 'Информация про Омский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      }
+    }
+  },
+  novosibirsk: {
+    name: "г. Новосибирск",
+    bubble: {
+      hostel: {
+        href: '#',
+        linkText: 'Новосибирский хостел',
+        text: 'Информация про Новосибирский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      },
+      apart: {
+        href: '#',
+        linkText: 'Новосибирский апарт-отель',
+        text: 'Информация про Новосибирский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      }
+    }
+  },
+  primor: {
+    name: "г. Владивосток",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Приморский апарт-отель',
+        text: 'Информация про Приморский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Приморский хостел',
+        text: 'Информация про Приморский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  chelyabinsk: {
+    name: "г. Челябинск",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Челябинский апарт-отель',
+        text: 'Информация про Челябинский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Челябинский хостел',
+        text: 'Информация про Челябинскй хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  ekaterinburg: {
+    name: "г. Екатеринбург",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Свердловский апарт-отель',
+        text: 'Информация про Свердловский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Свердловский хостел',
+        text: 'Информация про Свердловский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  ufa: {
+    name: "г. Уфа",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Уфимский апарт-отель',
+        text: 'Информация про Уфимский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Уфимский хостел',
+        text: 'Информация про Уфимский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  perm: {
+    name: "г. Пермь",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Пермский апарт-отель',
+        text: 'Информация про Пермский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Пермский хостел',
+        text: 'Информация про Пермский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  kazan: {
+    name: "г. Казань",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Казанский апарт-отель',
+        text: 'Информация про Казанский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Казанский хостел',
+        text: 'Информация про Казанский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  samara: {
+    name: "г. Самара",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Самарский апарт-отель',
+        text: 'Информация про Самарский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Самарский хостел',
+        text: 'Информация про Самарский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  nnovgorod: {
+    name: "г. Нижний Новгород",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Нижегородский апарт-отель',
+        text: 'Информация про Нижегородский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Нижегородский хостел',
+        text: 'Информация про Нижегородский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  volgograd: {
+    name: "г. Волгоград",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Волгоградский апарт-отель',
+        text: 'Информация про Волгоградский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Волгоградский хостел',
+        text: 'Информация про Волгоградский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  rostov: {
+    name: "г. Ростов-на-Дону",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Ростовский апарт-отель',
+        text: 'Информация про Ростовский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Ростовский хостел',
+        text: 'Информация про Ростовский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  voronej: {
+    name: "г. Воронеж",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Воронежский апарт-отель',
+        text: 'Информация про Воронежский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Воронежский хостел',
+        text: 'Информация про Воронежский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
+      }
+    }
+  },
+  stpeter: {
+    name: "г. Санкт-Петербург",
+    bubble: {
+      apart: {
+        href: '#',
+        linkText: 'Ленинградский апарт-отель',
+        text: 'Информация про Ленинградский апарт-отель Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum nonLorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus .'
+      },
+      hostel: {
+        href: '#',
+        linkText: 'Ленинградский хостел',
+        text: 'Информация про Ленинградский хостел Lorem ipsum dolor sit amet consectetur adipisicing elit. Et est esse optio porro alias consequuntur, accusamus exercitationem sed harum non.'
       }
     }
   }
 }
 
+
 var bubble = document.getElementById('bubble');
 var curReg = null;
 var curPath = null;
-
 
 var path = document.querySelector('svg');
 
 var deactivateRegion = function () {
   bubble.setAttribute('hidden', 'hidden');
-  bubble.style.top = 0;
-  bubble.style.left = 0;
+  // bubble.style.top = 0;
+  // bubble.style.left = 0;
   if (curPath) {
     curPath.classList.remove('active-region');
   }
 }
 
 path.onclick = function (e) {
+  deactivateRegion();
   curPath = e.target;
   regId = curPath.id;
   console.log(regId);
@@ -55,15 +266,22 @@ path.onclick = function (e) {
   if (regions[regId]) {
     curReg = regions[regId];
     curPath.classList.add('active-region');
-    // bubble.querySelector('.bubble').innerHTML = curReg.bubble;
+    bubble.querySelector('.bubble__link--apart').href = curReg.bubble.apart.href;
+    bubble.querySelector('.bubble__link--apart').innerText = curReg.bubble.apart.linkText;
+    bubble.querySelector('.bubble__info--apart').innerText = curReg.bubble.apart.text;
+    bubble.querySelector('.bubble__link--hostel').href = curReg.bubble.hostel.href;
+    bubble.querySelector('.bubble__link--hostel').innerText = curReg.bubble.hostel.linkText;
+    bubble.querySelector('.bubble__info--hostel').innerText = curReg.bubble.hostel.text;
+    bubble.querySelector('.bubble__name').innerText = curReg.name;
+
     bubble.removeAttribute('hidden');
 
-    bubble.style.top = e.pageY + 'px';
-    bubble.style.left = e.pageX + 'px';
+    // bubble.style.top = e.pageY + 'px';
+    // bubble.style.left = e.pageX + 'px';
   } else {
     deactivateRegion();
   }
 }
 
 var closeBubble = bubble.querySelector('.close-bubble');
-closeBubble.addEventListener('click', deactivateRegion) ;
+closeBubble.addEventListener('click', deactivateRegion);
